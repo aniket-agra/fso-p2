e36bdf19bef248da0c8d0e0435de1fe63b601310
Aniket Agrawal <aniket.mpa@gmail.com>
2023-10-06 21:42:10 +0200

    Add solution to exercise 4

--- courseInfo/src/components/App.jsx
+++ courseInfo/src/components/App.jsx
@@ -3,8 +3,8 @@ import { Course } from "./Course"
 const App = () => {
   const courses = [
     {
-      id: 1,
       name: 'Half Stack application development',
+      id: 1,
       parts: [
         {
           name: 'Fundamentals of React',
@@ -20,9 +20,14 @@ const App = () => {
           name: 'State of a component',
           exercises: 14,
           id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
         }
       ]
-    },
+    }, 
     {
       name: 'Node.js',
       id: 2,
@@ -39,9 +44,15 @@ const App = () => {
         }
       ]
     }
-  ]
+  ];
 
-  return <Course course={courses[0]} />
+  return (
+    <>
+      {
+        courses.map(course => <Course course = {course} key = {course.id}/>)
+      }
+    </>
+  );
 }
 
 export default App
