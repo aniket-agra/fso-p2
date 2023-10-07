d831fcab23dae6d4a14e5cf8d4597d47f4d44ee7
Aniket Agrawal <aniket.mpa@gmail.com>
2023-10-07 17:03:02 +0200

    Add solution to ex 6

--- phonebook/src/components/App.jsx
+++ phonebook/src/components/App.jsx
@@ -1,12 +1,45 @@
 import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  function updateNewName (e) {
+    setNewName(e.target.value);
+  }
+
+  function formHandler(e) {
+    e.preventDefault();
+    const newPerson = { name : newName};
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  }
 
   return (
-    <>
-    
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit = {formHandler}>
+        <div>
+          name: <input
+                  type = "text"
+                  value = {newName}
+                  onChange = {updateNewName}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {
+          persons.map((person, indx) => <li key = {indx}>{person["name"]}</li>)
+        }
+      </ul>
+      
+    </div>
   )
 }
 
