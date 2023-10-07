3ca901163f49536338ab0c483c6ed485f0d1f5f5
Aniket Agrawal <aniket.mpa@gmail.com>
2023-10-07 18:36:39 +0200

    Add solution to exercise 8

--- phonebook/src/components/App.jsx
+++ phonebook/src/components/App.jsx
@@ -2,20 +2,28 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      phone : "000"
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [number, setNumber] = useState("");
 
   function updateNewName (e) {
     setNewName(e.target.value);
   }
 
+  function updateNumber(e) {
+    setNumber(e.target.value);
+  }
+
   function formHandler(e) {
     e.preventDefault();
     if (!persons.map(person => person.name).includes(newName)) {
-      const newPerson = { name : newName};
+      const newPerson = { name : newName, phone : number};
       setPersons(persons.concat(newPerson));
       setNewName("");  
+      setNumber("");
     } else {
       alert(`${newName} is already added to phonebook`);
     }
@@ -33,13 +41,22 @@ const App = () => {
                 />
         </div>
         <div>
+          number: <input 
+                    type = "text"
+                    value = {number}
+                    onChange = {updateNumber}
+                  />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {
-          persons.map(person => <li key = {person["name"]}>{person["name"]}</li>)
+          persons.map(person => 
+            <li key = {person["name"]}>{person["name"]} {person["phone"]}</li>
+          )
         }
       </ul>
       
