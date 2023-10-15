ec3c9830f72cc26a01fd042ec7712ee56b78b90c
Aniket Agrawal <aniket.mpa@gmail.com>
2023-10-15 15:58:06 +0200

    Add search field

--- countries/src/components/App.jsx
+++ countries/src/components/App.jsx
@@ -1,7 +1,30 @@
 import { useState, useEffect } from "react";
 
+function Search({label, text, handler}) {
+    return (
+        <>
+            {label}: <input
+                        type = "text"
+                        value = {text}
+                        onChange = {handler}
+                    />
+        </>
+    )
+}
+
 function App () {
-    
+    const [query, setQuery] = useState("");
+
+    function updateSearch(e) {
+        console.log(query);
+        setQuery(e.target.value);
+    }
+
+    return (
+        <>
+            <Search label = "find countries" text = {query} handler = {updateSearch} />
+        </>
+    )
 }
 
 export default App
